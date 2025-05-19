317c3bab5c5335724bd2a43c85973e63fdef1c2c
mubanga-1 <mubanga.s10@gmail.com>
2025-05-18 18:48:25 -0700

    Add basic templates for todo lists and items

---
+++ src/todolist.js
@@ -0,0 +1,66 @@
+class Todolist {
+    constructor(name) {
+        this.name = name;
+        this.items = [];
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        if (name) {
+            this._name = name;
+        }
+    }
+
+    addItem(newItem) {
+        const titles = [];
+        this.items.forEach(item => {
+            titles.push(item.title.toLowerCase());
+        });
+
+        if (titles.includes(newItem.title.toLowerCase())) {
+            throw Error("Item already exists!");
+        }
+
+        this.items.push(newItem);
+    }
+
+    removeItem(item) {
+        if (this.items.includes(item)) {
+            const itemIndex = this.items.indexOf(item);
+            this.items.splice(itemIndex, itemIndex + 1);
+        }
+    }
+}    
+
+
+class Item {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(title) {
+        if (title && (typeof title === "string")) {
+            this._title = title;
+        } 
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    set description(description) {
+        if (description.length > 2) {
+            this._description = description;
+        }
+    }
+}
