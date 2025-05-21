1ff8ba4d0c324a5e6d17ea3069334108a72354c4
mubanga-1 <mubanga.s10@gmail.com>
2025-05-21 15:48:36 -0700

    Create mechanism for selecting first list after page load

--- src/content.js
+++ src/content.js
@@ -1,9 +1,4 @@
-import { createElement, appendChildren, clearElement } from "./utils.js";
-
-
-function displayAddForm(list) {
-
-}
+import { createElement, appendChildren, clearElement, highlight } from "./utils.js";
 
 
 // For displaying the contents of a specific todolist
@@ -14,8 +9,12 @@ function displayContent(list) {
     clearElement(contentContainer);
 
     // Create header and list elements 
-    const header = createElement({type: "div", id: "list-heading", classList: [], text: `${list.title}`});
+    const header = createElement({type: "div", id: "list-heading", classList: [], text: `${list.name}`});
     const listWrapper  = createElement({type: "ul", id: "list", classList: [], text: ""});
+
+    // Creating and deleting list items
+    const create = createElement({type: "div", id: "create-item", classList: ["list-item"], text: "+"});
+    const remove = createElement({type: "div", id: "remove-item", classList: ["list-item"], text: "-"});
 
     // Populate list 
     for (let i = 0; i < list.items; i++) {
@@ -28,4 +27,18 @@ function displayContent(list) {
 
 }
 
-export { displayContent };
+// Displays the items of the first todolist saved in local storage
+function displayFirst(names) {
+    const lists = JSON.parse(localStorage.getItem("lists"));
+
+    highlight(document.querySelector(`[data-name='${names[0]}']`), "h");   
+    displayContent(lists[names[0]]);
+}
+
+
+function displayAddForm(list) {
+
+}
+
+
+export { displayContent, displayFirst };
