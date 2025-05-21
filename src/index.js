b8956ef31dc3ebf8059e7f0200d453bf1ebb4667
mubanga-1 <mubanga.s10@gmail.com>
2025-05-21 07:29:17 -0700

    Fully implement the sidebar section of the page
    Implemented the notion of displaying contents of sidebar and creating
    and deleting projects. Addionality incorporated imported font, used
    appropriate spacing for elements and added preferred color properties.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,99 @@
+// Side effect import styles.css
 import "./styles.css";
-import { generateSideBar, highlight } from "./exports.js";
+
+// Import relays from exports
+import { highlight, displayElement, 
+createList, deleteList, getListNames, generateNav , Todolist } from "./exports.js";
+
+// Add lists array for containing todolist objects to local storage if not already there 
+const listsContainer = "lists";
+
+if (!localStorage.getItem(listsContainer)) {
+    localStorage.setItem(listsContainer, JSON.stringify({}));;
+}
 
 
-generateSideBar();
+// Get wrapper for create and delete buttons
+const modifiercontainer = document.querySelector("[data-name='modifier-wrapper']");
 
-const lists = document.querySelectorAll("[data-name='list']");
+// Form wrapper for input field and submit button 
+const formWrapper = document.querySelector("[data-name='name-input']");
+
+// Get input field and submit button for small form for creating new list and deleting old lists
+const nameInput = document.querySelector("[data-name='list-name']");
+const submitBtn = document.querySelector("[data-name='submit']");
+
+// Store the container, input field and submit button in array for better use
+const displayElements = [formWrapper, nameInput, submitBtn];
+
+// Used to determine whether to delete or create a todolist
+let operation;
 
 
+// Check which button was clicked apply appropriate functionality
+modifiercontainer.addEventListener("click", (event) => {
+    let target = event.target;
+    let btnNames = ["create", "delete"];
+
+    // If the id of the target clicked on is in btnNames display a small form
+    if (btnNames.includes(target.id)) {
+        operation = target.id;
+
+        // Display all display elements upon the detecting the user's click
+        displayElements.forEach(element => {
+            if (element.dataset.name === "name-input") {
+                displayElement(element, "v", "flex");
+                element.style.flexDirection = "column";
+                element.style.gap = "1rem";
+
+            }
+            else displayElement(element, "v");
+        });      
+
+        submitBtn.innerText = target.id;
+    }
+});
+
+// When submit button is clicked create or delete list if nameInput is given a valid value
+submitBtn.addEventListener("click", () => {
+
+    const listNames = getListNames();
+
+    // If the value of the nameInput is not empty create or delete list mentioned  
+    if (nameInput.value) {
+        if (operation === "create") {
+            if (!listNames.includes(nameInput.value)) createList(nameInput.value);
+            else alert(`${nameInput.value} already exists!!`);
+
+        } else {
+            if (listNames.includes(nameInput.value)) deleteList(nameInput.value);
+            else alert("List does not exist!");
+
+        }
+        
+        // Clear input fields and hide form
+        nameInput.value = "";
+        displayElements.forEach(element => displayElement(element, "i"));
+
+        // Regenerate the nav-links
+        generateNav();
+
+    // Else alert the user to enter a name into the input field
+    } else (
+        window.alert("Enter list name!")
+    )
+});
+
+// Get elements that contain list names
+const listElements = document.querySelectorAll("[data-name='list']");
+
+// Functionality for visually toggling between list options
 document.querySelector("[data-name='nav-link-wrapper']").addEventListener("click", (event) => {
     let target = event.target;
 
-    for (let i = 0; i < lists.length; i++) {
-        let currentList = lists[i];
+    // Highlight target and unhighlight previous target
+    for (let i = 0; i < listElements.length; i++) {
+        let currentList = listElements[i];
 
         if (target === currentList) {
             highlight(currentList, "h");
--- src/sidebar.js
+++ src/sidebar.js
@@ -1,38 +1,65 @@
 // Import createElement and appendChildren function from utils.js
-import { createElement, appendChildren } from "./utils.js";
+import { createElement, appendChildren, clearElement, getListNames } from "./utils.js";
 
-const todolists = ["todolist1", "todolist2", "todolist3", "todolist4", "todolist5"];
+// Used to generate navigation links
+function generateNav() {
+    const lists = getListNames();
+    const navLinks = document.querySelector("#nav-links");
+    clearElement(navLinks);
 
+    for (let list of lists) {
+        const newListButton = createElement({type: "div", id: "", classList: ["list"], text: `${list}`});
+        newListButton.dataset.name = "list";
+        navLinks.appendChild(newListButton);
 
+    }
+
+}
+
+// Used to display the todolists and other elements of the sidebar
 function generateSideBar() {
+    // Select sidebar element from template.html
     const sidebar = document.querySelector('[data-name="nav-bar"]');
 
+    // Create header section for sidebar
     const header = createElement({type: "div", id: "nav-header", classList: [], text: ""});
     const heading = createElement({type: "div", id: "nav-heading", classList: [], text: "Projects"});
     const modifier = createElement({type: "div", id: "modifiers", classList: [], text: ""});
+    modifier.dataset.name = "modifier-wrapper";
+
+    // Input container for input used for creating or deleting todolists
+    const listNameContainer = createElement({type: "div", id: "name-input", classList: ["input-wrapper"], text: ""});
+    listNameContainer.dataset.name = "name-input"; 
+
+    // Input used to name new list or name list to delete
+    const listName = createElement({type: "input", id: "list-name", classList: [], text: ""});
+    listName.setAttribute("autocomplete", "off");
+    listName.dataset.name = "list-name";
+
+    const submitBtn = createElement({type: "button", id: "submit-name", classList: ["submit-btn"], text: ""});
+    submitBtn.dataset.name = "submit"
+
+    appendChildren(listNameContainer, [listName, submitBtn]);
+
+    // Add heading and modifier to header section
     appendChildren(header, [heading, modifier]);
     
-
-    const add = createElement({type: "div", id: "create", classList: [], text: "+"});
-    const remove = createElement({type: "div", id: "delete", classList: [], text: "-"});
+    // Add buttons for adding and removing todolists to 
+    const add = createElement({type: "button", id: "create", classList: [], text: "+"});
+    const remove = createElement({type: "button", id: "delete", classList: [], text: "-"});
     appendChildren(modifier, [remove, add]);
 
 
-    sidebar.appendChild(header);
+    appendChildren(sidebar, [header, listNameContainer]);
 
-
-
+    // Add navigation element wrapper in to sidebar
     const navElementWrapper = createElement({type: "div", id: "nav-links", classList: [], text: ""});
     navElementWrapper.dataset.name = "nav-link-wrapper";
     sidebar.appendChild(navElementWrapper);
 
-    for (let i = 0; i < todolists.length; i++) {
-        const newListButton = createElement({type: "div", id: "", classList: ["list"], text: `${todolists[i]}`});
-        newListButton.dataset.name = "list";
-        navElementWrapper.appendChild(newListButton);
-
-    }
+    generateNav();
 }
 
+generateSideBar();
 
-export { generateSideBar };
+export { generateSideBar, generateNav };
